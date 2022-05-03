0555bb4033bb4630857d84ea2d17cf6d83b67d3c
Sebastian Mora <chebimora02@gmail.com>
2022-05-03 16:17:51 -0500

    Test of Unity LogIn 5

--- LoginUnity.php
+++ LoginUnity.php
@@ -1,7 +1,28 @@
 <?php
     if($_POST){
-        $data = $_POST["userData"];
+        $data = $_POST["dataPacket"];
         $dataJson = json_decode($data, true);
-        echo "Json:" . $dataJson["name"] . "\n";
+
+        $username = $dataJson["name"];
+        $password = $dataJson["password"];
+        
+        $servername = "localhost"; //Chamge for the server MySQL config
+        $username = "API";
+        $password = "12345";
+        $dbname = "BeatDancer";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+        $userCheck = "SELECT * FROM Player WHERE username = '$username' AND pswd = '$password'";
+        $rs = mysqli_query($conn,$userCheck);
+        $existent = mysqli_fetch_array($rs, MYSQLI_NUM);
+        if(is_null($existent)){
+            echo "Something went wrong";
+        }
+        else{
+            echo "LogIn Succesfull";
+        }
     }
 ?>
